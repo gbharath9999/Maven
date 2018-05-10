d72d04e5316ce33d41710e33a5298f6969151511
bharath <gbharath9999@gmail.com>
2018-05-10 17:12:52 +0530

    bharath

---
+++ src/test/java/com/maven/pratice/SampleTest.java
@@ -0,0 +1,20 @@
+package com.maven.pratice;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void m1(){
+		
+		System.out.println("hi............m1");
+		
+	}
+	@Test
+	public void m2(){
+		
+		System.out.println("hi............m2");
+		
+	}
+	
+}
